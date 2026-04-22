d9154f0671b867e0db030c39177d198f2ff9febf
omar <omarmonzer88@gmail.com>
2026-04-22 04:04:02 +0200

    omar

---
+++ src/components/Banner.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.png";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "NetEng", "SecOps", "DevOps" ];
+  const period = 500;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <h1>{`I'm Omar,`} <span className="txt-rotate" dataPeriod="2000" data-rotate='[ "NetEng", "SecOps", "DevOps" ]'><span className="wrap">{text}</span></span></h1>
+            <p>Highly motivated Network Engineer with a strong foundation in designing, implementing, and troubleshooting complex network infrastructures. Certified in CCNA and AWS Certified Cloud Practitioner, with a passion for automating routine tasks using Python and building secure enterprise networks. Dedicated to creating reliable, high-performance connectivity solutions, specializing in Cisco environments, SD-WAN, and cloud hybrid setups.</p>
+            <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={30} /></button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img"/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
