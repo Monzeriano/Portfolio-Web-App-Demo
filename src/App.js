95d4957dbbf90acda82ce6d92b2b816e5825591a
omar <omarmonzer88@gmail.com>
2026-04-29 13:10:54 +0300

    Footer Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import NavBar from './components/NavBar';
 import {Banner} from './components/Banner';
 import { Skills} from './components/Skills';
 import { Projects } from './components/Projects'
+import { Contact } from './components/Contact'
+import { Newsletter } from './components/Newsletter'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-
+      <Contact />
+      <Newsletter />
+      <Footer />
     </div>
   );
 }
